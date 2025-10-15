i-dot-ai/minute

feat: combine consecutive citations

common/services/transcription_handler_service.py
@@ -12,6 +12,7 @@ from common.prompts import get_chat_with_transcript_system_message
 from common.services.exceptions import InteractionFailedError, TranscriptionFailedError
 from common.services.transcription_services.transcription_manager import TranscriptionServiceManager
 from common.settings import get_settings
+from common.templates.add_citations import combine_consecutive_citations
 from common.types import DialogueEntry, TranscriptionJobMessageData
 
 settings = get_settings()
@@ -76,6 +77,7 @@ class TranscriptionHandlerService:
                         )
 
                 chat_response = await chatbot.chat(messages=chat_history)
+                chat_response = combine_consecutive_citations(chat_response)
                 chat.assistant_content = chat_response
                 chat.status = JobStatus.COMPLETED
 

common/templates/default/delivery.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 from common.database.postgres_models import DialogueEntry, Minute
 from common.llm.client import FastOrBestLLM, create_default_chatbot
 from common.prompts import get_transcript_messages
-from common.templates.template_utils import add_citations_to_minute
+from common.templates.add_citations import add_citations_to_minute
 from common.templates.types import Template
 from common.types import AgendaUsage, MinuteAndHallucinations
 

common/templates/types.py
@@ -4,7 +4,7 @@ from common.database.postgres_models import DialogueEntry, Minute
 from common.llm.client import FastOrBestLLM, create_default_chatbot
 from common.prompts import get_section_for_agenda_prompt, string_to_system_message
 from common.settings import get_settings
-from common.templates.template_utils import add_citations_to_minute
+from common.templates.add_citations import add_citations_to_minute
 from common.types import AgendaUsage, MinuteAndHallucinations
 
 settings = get_settings()

communitiesuk/i-ai-minute

common/services/transcription_handler_service.py
@@ -12,6 +12,7 @@ from common.prompts import get_chat_with_transcript_system_message
 from common.services.exceptions import InteractionFailedError, TranscriptionFailedError
 from common.services.transcription_services.transcription_manager import TranscriptionServiceManager
 from common.settings import get_settings
+from common.templates.citations import combine_consecutive_citations
 from common.types import DialogueEntry, TranscriptionJobMessageData
 
 settings = get_settings()
@@ -76,6 +77,7 @@ class TranscriptionHandlerService:
                         )
 
                 chat_response = await chatbot.chat(messages=chat_history)
+                chat_response = combine_consecutive_citations(chat_response)
                 chat.assistant_content = chat_response
                 chat.status = JobStatus.COMPLETED
 

common/templates/default/delivery.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 from common.database.postgres_models import DialogueEntry, Minute
 from common.llm.client import FastOrBestLLM, create_default_chatbot
 from common.prompts import get_transcript_messages
-from common.templates.template_utils import add_citations_to_minute
+from common.templates.citations import add_citations_to_minute
 from common.templates.types import Template
 from common.types import AgendaUsage, MinuteAndHallucinations
 

common/templates/types.py
@@ -4,7 +4,7 @@ from common.database.postgres_models import DialogueEntry, Minute
 from common.llm.client import FastOrBestLLM, create_default_chatbot
 from common.prompts import get_section_for_agenda_prompt, string_to_system_message
 from common.settings import get_settings
-from common.templates.template_utils import add_citations_to_minute
+from common.templates.citations import add_citations_to_minute
 from common.types import AgendaUsage, MinuteAndHallucinations
 
 settings = get_settings()