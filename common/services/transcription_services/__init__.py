b3e569303a5ba3f2d0b66d1e1e1f6e827bb2340f
Patryk.Kuchta <patryk.kuchta@softwire.com>
2026-01-26 15:51:06 +0000

    fix: imports for transcription adapters

--- common/services/transcription_services/__init__.py
+++ common/services/transcription_services/__init__.py
@@ -2,5 +2,6 @@ from .adapter import TranscriptionAdapter
 from .aws import AWSTranscribeAdapter
 from .azure import AzureSpeechAdapter
 from .azure_async import AzureBatchTranscriptionAdapter
+from .whisply_local import WhisplyLocalAdapter
 
-__all__ = ["AWSTranscribeAdapter", "AzureBatchTranscriptionAdapter", "AzureSpeechAdapter", "TranscriptionAdapter"]
+__all__ = ["AWSTranscribeAdapter", "AzureBatchTranscriptionAdapter", "AzureSpeechAdapter", "TranscriptionAdapter", "WhisplyLocalAdapter"]
