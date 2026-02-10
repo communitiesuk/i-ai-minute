f1e8f7cd568c904d41ae8d5cbe3816641dd257fa
Patryk.Kuchta <patryk.kuchta@softwire.com>
2026-02-10 14:49:00 +0000

    chore: autolint

--- evals/transcription/src/constants.py
+++ evals/transcription/src/constants.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 
-from common.constants import STEREO_CHANNELS, TARGET_SAMPLE_RATE
-
 WORKDIR = Path(__file__).resolve().parent
 CACHE_DIR = WORKDIR / "cache"
 CACHE_DIR.mkdir(exist_ok=True)
