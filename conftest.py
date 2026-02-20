6545720ce45b0b91531c79c9be6d7d0e8644607f
Toluwalope <toluwalopelawal1@DGTL0007.local>
2026-02-20 11:41:18 +0000

    chore: AIILG-198 : export env vars to shell

--- conftest.py
+++ conftest.py
@@ -3,10 +3,6 @@ import asyncio
 import pytest
 from dotenv import load_dotenv
 
-load_dotenv(".env")
-load_dotenv(".env.example")
-
-
 @pytest.fixture(scope="session")
 def event_loop_policy() -> asyncio.AbstractEventLoopPolicy:
     return asyncio.get_event_loop_policy()
